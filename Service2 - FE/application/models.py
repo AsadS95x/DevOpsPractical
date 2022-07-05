d511ccdfdb505f667638f03fc1696cd6443faf28
AsadS <asads95@live.co.uk>
2022-07-05 01:25:34 +0000

    Models.[y update

---
+++ Service2 - FE/application/models.py
@@ -0,0 +1,12 @@
+from application import db
+
+class Houses(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    house_name = db.Column(db.String(50))
+    house_location = db.Column(db.String(50))
+    house_size = db.Column(db.Integer)
+    house_price = db.Column(db.Integer)
+    date_generated = db.Column(db.DateTime)
+    def __str__(self):
+        return f"{self.house_name}: {self.house_location} with {self.house_size} bedrooms, would cost around {self.house_price}"
+        
