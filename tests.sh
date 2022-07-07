d20ebe393900ba3778bf98bad0505e3ae5ff1adf
AsadS <asads95@live.co.uk>
2022-07-07 22:44:10 +0000

    tests update

---
+++ tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#!/bin/bash
+
+declare -a dirs=(Service2-FE Service3-Loc Service4-Size Service5-Price)
+
+for dir in ${dirs[@]}; do
+    cd ${dir}
+    python3 -m venv venv
+    source venv/bin/activate
+    pip3 install -r requirements.txt
+    python3 -m pytest --cov=application --cov-report=html
+    deactivate
+    cd ..
+done
