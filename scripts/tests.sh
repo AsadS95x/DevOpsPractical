a3a678470b10cf65a53335a10fa6c694ef4037e3
AsadS <asads95@live.co.uk>
2022-07-08 00:39:03 +0000

    fixing test

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -2,13 +2,13 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y curl jq python3-dev libpq-dev software-properties-common
+sudo apt-get install -y curl jq python3-.10-venv
 
 declare -a dirs=(Service2-FE Service3-Loc Service4-Size Service5-Price)
 
 for dir in ${dirs[@]}; do
     cd ${dir}
-    python3 -m venv venv
+    sudo python3 -m venv venv
     source venv/bin/activate
     pip3 install -r requirements.txt
     python3 -m pytest '''--cov=application --cov-report=html'''
