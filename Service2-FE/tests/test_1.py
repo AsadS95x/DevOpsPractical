f4d7e8783cce26f1640d5ce97c26bd3c976baff8
AsadS <asads95@live.co.uk>
2022-07-08 09:56:54 +0000

    typos

--- Service2-FE/tests/test_1.py
+++ Service2-FE/tests/test_1.py
@@ -14,9 +14,9 @@ class TestResponse(TestBase):
     def test_index(self):
 
         with mock() as m:
-            m.get('http://housesize:5000/get_size', text=5)
+            m.get('http://housesize:5000/get_size', text='5')
             m.get('http://houselocation:5000/get_location', text='Beverly Hills')
-            m.post('http://houseprice:5000/get_price', text=325000)
+            m.post('http://houseprice:5000/get_price', text='325000')
 
             response = self.client.get(url_for('index'))
 
