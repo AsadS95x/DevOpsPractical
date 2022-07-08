a3f2729b7b3dc605beb432e36b260196e51b25a1
AsadS <asads95@live.co.uk>
2022-07-08 10:55:37 +0000

    .

--- Service4-Size/tests/test_3.py
+++ Service4-Size/tests/test_3.py
@@ -1,6 +1,7 @@
 from flask import url_for
 from flask_testing import TestCase
 from unittest.mock import patch
+import random
 import pytest
 from application import app
 
@@ -12,8 +13,9 @@ class TestBase(TestCase):
 class TestViews(TestBase):
 
     def test_get_size(self):
-       with mock('random.randrange(1,7)') as r:
+       with patch('random.randrange') as r:
            r.return_value = 5
+           #house_size=5
            response = self.client.get(url_for('size'))
            self.assertEqual(response.status_code, 200)
            self.assertIn(b'5', response.data)
